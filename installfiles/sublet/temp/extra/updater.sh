ab074440b93769f2c95e2de14c39ca5dcddfaf20
Jack-John-Joe <131839749+Jack-John-Joe@users.noreply.github.com>
2024-04-13 18:12:08 -0500

    it works-ish
    fixed up some links

--- installfiles/sublet/temp/extra/updater.sh
+++ installfiles/sublet/temp/extra/updater.sh
@@ -1,7 +1,7 @@
 echo "Updating Sublet.."
 cd /sublet/extras
 rm ./db.xml
-wget # the githubusercontent thing
+wget https://github.com/Jack-John-Joe/sublet/raw/main/database/db/db.xml
 cp ./db.xml /sublet/config
 wget # the newest sublet ver
 rm /sbin/sublet.x
