024cd2d4e7a9d96948816822afa907b3b6c5f92b
Jack-John-Joe <131839749+Jack-John-Joe@users.noreply.github.com>
2024-04-13 19:19:41 -0500

    fixing the runner

---
+++ build.sh
@@ -0,0 +1,13 @@
+sudo apt-get install shc
+sudo apt install ca-certificates gnupg
+sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-devel
+shc -f sublet.sh
+unzip installfiles.zip -d ./installfiles/
+rm -rf ./installfiles.zip
+cp -a sublet.x ./installfiles/sublet/temp
+mcs -out:sublet sublet.cs
+cp -a sublet ./installfiles/sublet/temp/extras
+zip -r zip myfile.zip ./installfiles/
